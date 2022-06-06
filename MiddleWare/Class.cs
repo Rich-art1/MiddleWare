39e30e4b0ae98975edbcc6620506e1434181cdb0
student <student@DESKTOP-43L7716>
2022-06-06 10:25:37 +0200

    Dodaj pliki projektów.

---
+++ MiddleWare/Class.cs
@@ -0,0 +1,38 @@
+﻿using Shyjus.BrowserDetection;
+
+namespace MiddleWare
+{
+    public class MyCustomMiddleware
+    {
+        private RequestDelegate next;
+
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext,
+                                      IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.InternetExplorer)
+            {
+                      httpContext.Response.Redirect("mozilla.org/pl/firefox/new/");
+            }
+            else
+            {
+                await this.next.Invoke(httpContext);
+            }
+        }
+
+
+        public static class MyCustomMiddlewareExtension
+        {
+            public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder builder)
+            {
+                return builder.UseMiddleware<MyCustomMiddleware>();
+            }
+        }
+    }
+}
